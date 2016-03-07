ae42ce0344b2c8aa3cf2d6d08ec45a12dac71f60
Jonathan Kunstwald <mail@jonathankunstwald.de>
2016-03-07 14:44:53 +0100

    Suggestions and Autocomplete

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 
 
 $(function() {
-    $('#clip-input').attr('placeholder', 'Enter a Command');
+    $('#clip-input')
+        .attr('placeholder', 'Enter a Command')
+        .on('input', function() {
+            core.updateSuggestions();
+        });
+
+    $(document).keydown(function(e) {
+        switch (e.which) {
+            case 9:
+                e.preventDefault();
+                core.useFirstSuggestion();
+                break;
+        }
+    });
 });
 
 
@@ -13,7 +26,34 @@ function parseInput(overrideInput) {
     utility.openTab('http://' + receivedUrl);
 
     $('#clip-input').val('').focus();
+    this.updateSuggestions();
 }
+
+var core = {
+
+    suggestions: [],
+
+    updateSuggestions: function() {
+        var inputString = $('#clip-input').val(),
+            argumentArray = inputString.split(' ');
+        this.suggestions = groupManager.getSuggestion(argumentArray);
+
+        $('#clip-suggestions').html('');
+        $.each(this.suggestions, function(i, obj) {
+            $('<div/>', { class: 'clip-suggestion' })
+                .html(obj.split(inputString).join('<span class="clip-highlight">' + inputString + '</span>'))
+                .appendTo('#clip-suggestions');
+        });
+    },
+
+    useFirstSuggestion: function() {
+        if (this.suggestions[0]) {
+            $('#clip-input').val(this.suggestions[0]);
+            this.updateSuggestions();
+        }
+    }
+
+};
 
 var groupManager = {
 
@@ -38,13 +78,15 @@ var groupManager = {
                 rhz: 'rhz',
                 brettener: 'brettener-woche',
                 moz: 'moztrauer',
-                nrc: 'nrc'
+                nrc: 'nrc',
+                _default: 'vrsmedia'
             },
             var2: {
                 local: '.de.local',
                 live: '.de',
                 beta: '-beta.de',
-                test: '-test.de'
+                test: '-test.de',
+                _default: '.de.local'
             }
         }
 
@@ -67,10 +109,39 @@ var groupManager = {
             var varTarget = '{$' + obj + '}',
                 varValue = this.groups[groupName][obj][variables[i]];
 
+            if (!varValue) varValue = this.groups[groupName][obj]['_default'];
             outputUrl = outputUrl.replace(varTarget, varValue);
         }.bind(this));
 
         return outputUrl;
+    },
+
+    getSuggestion: function(args) {
+        var matches = [],
+            inputLength,
+            inputString;
+
+        if (args.length === 1 && args[0].length) {
+            inputLength = args[0].length;
+            inputString = args[0];
+
+            $.each(this.groups.default.var1, function(i, obj) {
+                if (i === '_default') return true;
+                if (i.substr(0,inputLength) === inputString) matches.push(i + ' ');
+            });
+        } else if (args.length === 2) {
+            if (!this.groups.default.var1[args[0]]) return [];
+
+            inputLength = args[1].length;
+            inputString = args[1];
+
+            $.each(this.groups.default.var2, function(i, obj) {
+                if (i === '_default') return true;
+                if (i.substr(0,inputLength) === inputString) matches.push(args[0] + ' ' + i);
+            });
+        }
+
+        return matches;
     }
 
 
